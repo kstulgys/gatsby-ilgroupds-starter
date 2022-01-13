3f93a591e0f39b2a06bc7a90c1e5782d6a391254
kstulgys <karolis.stulgys@gmail.com>
2022-01-13 12:30:13 +0000

    wip

---
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -0,0 +1,11 @@
+// import { extendTheme } from "@chakra-ui/react"
+// const theme = {
+//   colors: {
+//     primary: "rebeccapurple",
+//   },
+// }
+
+// export default extendTheme(theme)
+
+import { theme } from '@irishlife/ilgroupdesignsystem.theme.theme';
+export default theme;
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Button } from '@irishlife/ilgroupdesignsystem.atoms.button';
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <Button>Hello</Button>
+    </main>
+  );
+};
+
+export default IndexPage;
